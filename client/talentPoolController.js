2d23ffdef7db27944cbe2f6fc704a3c2efb896eb
oliverbrummel <oliverbrummel1@gmail.com>
2016-05-19 14:38:03 -0500

    messes around with modals on talentpool view/controller

--- client/talentPoolController.js
+++ client/talentPoolController.js
@@ -1,5 +1,7 @@
 angular.module('connectUApp')
   .controller('TalentPoolController', ['$http', 'ConnectUService', function($http, ConnectUService){
+    var vm = this;
 
+    vm.user4 = ConnectUService.user4;
 
   }]);//closes controller
