ce0eec9ee51cb04cc4dc64591fdb270776830fa5
suzannaaltman <suzannaaltman@gmail.com>
2016-05-19 10:05:54 -0500

    adds user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  email: {type: String, required: true, unique: true},
+  first_name: {type: String, required: true},
+  last_name: {type: String, required: true},
+  city: {type: String},
+  state: {type: String},
+  phone: {type: String},
+  high_school: {type: String},
+  college: {type: String},
+  current_workplace: {type: String},
+  slack_id: {type: String, required: true},
+  linkedin_url: {type: String},
+  focus: {type: String},
+  quote: {type: String},
+  seeking_internship: {type: Boolean},
+  seeking_employment: {type: Boolean},
+  admin: {type: Boolean, default: false},
+  internship: {[{site: String, year: Number}]},
+  photo_url: {type: String},
+
+
+})
+
+var userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
