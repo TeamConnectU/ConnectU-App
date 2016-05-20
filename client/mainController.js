282825c00f6ab069c8c3b85f4f244e349a5653b2
suzannaaltman <suzannaaltman@gmail.com>
2016-05-20 09:41:51 -0500

    adds show and hide functionality for header nav

--- client/mainController.js
+++ client/mainController.js
@@ -1,4 +1,10 @@
 angular.module('connectUApp')
   .controller('MainController', ['$http', function($http){
 
+    var vm = this;
+
+// if vm.loggedIn = true - shows Profle and Log Out, if vm.loggedIn = false - shows Alumni login
+// MUST ADD IF STATEMENT TO CHANGE TRUE/FALSE
+    vm.loggedIn = false;
+
   }]);
