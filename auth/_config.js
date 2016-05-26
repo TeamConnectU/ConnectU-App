d0678731e45ce203a028ab63702c8ae01966c2c5
oliverbrummel <oliverbrummel1@gmail.com>
2016-05-26 10:30:26 -0500

    add slack credentials to auth/_config.js and city/state to display on table

--- auth/_config.js
+++ auth/_config.js
@@ -2,8 +2,14 @@ var ids = {
   linkedin: {
     clientID: '773a8a5y9gfyug',
     clientSecret: 'V3KSkOqbhSUHUmzv',
-    callbackURL: "http://localhost:3000/auth/linkedin/callback"
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
   },
+
+  slack: {
+    clientID: '3545121647.45804055680',
+    clientSecret: 'f51c90e6038832cbd5d4efcbd85a76e2',
+    redirectURL: 'https://localhost:3000/auth'
+  }
 };
 
 module.exports = ids;
